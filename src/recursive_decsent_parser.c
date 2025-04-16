abaeb8f5056d45ea807cd658c665039560465fa7
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-04-16 22:06:10 +0200

    removed pipeline big process, maybe a mystake

--- src/recursive_decsent_parser.c
+++ src/recursive_decsent_parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   recursive_decsent_parser.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecasalin <ecasalin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kura <kura@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 06:55:11 by ecasalin          #+#    #+#             */
-/*   Updated: 2025/04/15 15:59:09 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/04/16 21:45:31 by kura             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,11 @@ typedef	struct s_pipes_data
 	char	is_init;
 }				t_pipes_data;
 
-typedef	struct s_special_var
+typedef	struct s_var_data
 {
-	int	last_cmd_ext_code;
-}				t_special_var;
+	char	**env;
+	int		last_cmd_ext_code;
+}				t_var_data;
 
 int	open_pipes(t_pipes_data *pipes)
 {
@@ -65,7 +66,7 @@ int	open_pipes(t_pipes_data *pipes)
 int	close_pipe(int *pipe[2])
 {
 	int	close_return;
-	
+
 	close_return = 0;
 	close_return = close(*pipe[0]);
 	close_return = close(*pipe[1]);
@@ -85,15 +86,15 @@ int	reset_pipes(t_pipes_data *pipes)
 	return (0);
 }
 
-/*while exec cmd 2 check if prev->prev->op is | : if it is, we good, if not, need to redirect cmd to stdout and set all pipes to -1 */
+/*while exec cmd 2 check if prev->prev->op is | : if it is, we good, if not, need to redirect cmd to stdout and set all pipes to -1 AND wait for all childs and return*/
 /*if curr.op is | and prev is NULL, or perv.op is &&/||, we need to create a new process for the pipeline coming */
 
-int	prepare_exec_left(t_pipes_data *pipes, t_node *curr_node, t_special_var *special_var)
+int	prepare_exec_left(t_pipes_data *pipes, t_node *curr_node, t_var_data *vars)
 {
 	int	child_pid;
-	
+
 	if (curr_node->left)
-		return (recursive_descent_parser(curr_node->left, special_var));
+		return (recursive_descent_parser(curr_node->left, vars));
 	if (curr_node->prev && curr_node->prev->op == PIPE)
 		open_pipes(pipes);
 	child_pid = fork();
@@ -103,15 +104,15 @@ int	prepare_exec_left(t_pipes_data *pipes, t_node *curr_node, t_special_var *spe
 		set_redirections();
 		exec();
 	}
-	
+
 }
 
-int	prepare_exec_right(t_pipes_data *pipes, t_node *curr_node, t_special_var *special_var)
+int	prepare_exec_right(t_pipes_data *pipes, t_node *curr_node, t_var_data *vars)
 {
 	if (curr_node->right)
-		special_var->last_cmd_ext_code = recursive_descent_parser(curr_node->right, special_var);
+		vars->last_cmd_ext_code = recursive_descent_parser(curr_node->right, vars);
 	else
-		return (exec(special_var->last_cmd_ext_code, &pipes, curr_node));
+		return (exec(vars->last_cmd_ext_code, &pipes, curr_node));
 }
 
 int	check_right_cmd_conditions(t_node *curr_node, int left_cmd_return)
@@ -127,25 +128,14 @@ int	check_right_cmd_conditions(t_node *curr_node, int left_cmd_return)
 	return (1);
 }
 
-int	recursive_descent_parser(t_node *curr_node, t_special_var *special_var)
+int	recursive_descent_parser(t_node *curr_node, t_var_data *vars)
 {
 	static t_pipes_data	pipes;
-	int					pipeline_pid;
-	int					pipeline_exit;
-	
-	pipeline_pid = 0;
+
 	if (!pipes.is_init)
 		pipes = (t_pipes_data){{-1, -1}, {-1, -1}, 0, 1};
-	if (curr_node->op == PIPE && !curr_node->prev || curr_node->prev->op != PIPE)
-		pipeline_pid = fork();
-	if (pipeline_pid != CHILD)
-	{
-		waitpid(pipeline_pid, &pipeline_exit, 0);
-		special_var->last_cmd_ext_code = get_exit_status(pipeline_exit);
-		return (special_var->last_cmd_ext_code);
-	}
-	special_var->last_cmd_ext_code = prepare_exec_left(&pipes, curr_node, special_var);
-	if (check_right_cmd_conditions(curr_node, special_var->last_cmd_ext_code))
-		return (prepare_exec_right(&pipes, curr_node, special_var));
-	return (special_var->last_cmd_ext_code);
+	vars->last_cmd_ext_code = prepare_exec_left(&pipes, curr_node, vars);
+	if (check_right_cmd_conditions(curr_node, vars->last_cmd_ext_code))
+		return (prepare_exec_right(&pipes, curr_node, vars));
+	return (vars->last_cmd_ext_code);
 }
