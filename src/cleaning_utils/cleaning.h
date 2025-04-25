c45489b68708ff2bc3cb04924662353d348f5ec9
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-04-25 18:33:29 +0200

    added general cleaning utils folder: clean.h and cleaning .c

--- src/cleaning_utils/cleaning.h
+++ src/cleaning_utils/cleaning.h
@@ -6,13 +6,16 @@
 /*   By: kura <kura@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 17:59:52 by kura              #+#    #+#             */
-/*   Updated: 2025/04/25 18:00:16 by kura             ###   ########.fr       */
+/*   Updated: 2025/04/25 18:30:54 by kura             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLEANING_H
 # define CLEANING_H
 
-
+void	*free_array(char **array);
+int		close_fd(int *fd);
+int		close_pipe(int *pipe);
+int		close_pipe_perror(char *err_msg, int return_value, int *pipe);
 
 #endif
