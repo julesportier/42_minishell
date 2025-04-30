075e524348cf1a97bdf62c6456ef76bec6324c4a
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-04-30 14:33:20 +0200

    export builtin works with var= , but segfaults if we exports a var without the '='

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecasalin <ecasalin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ecasalin <ecasalin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:41:28 by kura              #+#    #+#             */
-/*   Updated: 2025/04/28 12:28:03 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/04/30 11:57:03 by ecasalin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
+/*BUILTINS*/
 int	ms_echo(char **args);
-/*TEMP*/
+int	ms_export(char **args, char **env);
+
+/*BUILTINS UTILS*/
 char	*get_env_var(char *var_name, char **env);
-static char	**add_var_to_env(char **env, char *var);
 char	*get_env_var_value(char *var_name, char **env);
 
 #endif
--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecasalin <ecasalin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ecasalin <ecasalin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 15:41:16 by kura              #+#    #+#             */
-/*   Updated: 2025/04/28 16:53:27 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/04/30 14:31:12 by ecasalin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,120 +14,177 @@
 #include "../../libft/src/libft.h"
 #include "../error_handling/errors.h"
 #include "../general_utils/utils.h"
+#include "../shell_init/init.h"
+#include "../cleaning_utils/cleaning.h"
+#include "../minishell.h"
 
 // /*FAUT PASSER LE = AUSSI*/
-// char	*get_env_var(char *var_name, char **env)
-// {
-// 	int		i;
-	
-// 	if (env == NULL || var_name == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	while (env[i])
-// 	{
-// 		if (ft_strncmp(env[i], var_name, ft_strlen(var_name)) == SUCCESS)
-// 			return (env[i]);
-// 		i++;
-// 	}
-// 	return (NULL);
-// }
+char	*get_env_var(char *var_name, char **env)
+{
+	int		i;
 
-// char	*get_env_var_value(char *var_name, char **env)
-// {
-// 	char	*var_value;
-// 	int		i;
-	
-// 	var_value = get_env_var(var_name, env);
-// 	if (var_value == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	while (var_value[i] != '=')
-// 		i++;
-// 	return (&var_value[i + 1]);
-// }
+	if (env == NULL || var_name == NULL)
+		return (NULL);
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], var_name, ft_strlen(var_name)) == SUCCESS)
+			return (env[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_env_var_value(char *var_name, char **env)
+{
+	char	*var_value;
+	int		i;
+
+	var_value = get_env_var(var_name, env);
+	if (var_value == NULL)
+		return (NULL);
+	i = 0;
+	while (var_value[i] != '=')
+		i++;
+	return (&var_value[i + 1]);
+}
 
 // /*POUR RAJOUTER DU PIQUANT, SI VAR = NULL CA SEGFAULT*/
-// static char	**add_var_to_env(char **env, char *var)
-// {
-// 	int		i;
-// 	char	**new_env_array;
+static char	**add_var_to_env(char **env, char *var)
+{
+	int		i;
+	char	**new_env_array;
 
-// 	i = count_array_len(env);
-// 	new_env_array = malloc((i + 2) * sizeof(char *));
-// 	if (new_env_array == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	while (env[i])
-// 	{
-// 		new_env_array[i] = env[i];
-// 		i++;
-// 	}
-// 	new_env_array[i] = ft_strdup(var);
-// 	if (new_env_array[i] == NULL)
-// 	{
-// 		free_array(new_env_array);
-// 		return (NULL);
-// 	}
-// 	new_env_array[i + 1] = NULL;
-// 	free_array(env);
-// 	return (new_env_array);
-// }
-int	print_sorted_env(char **env)
+	i = count_array_len(env);
+	new_env_array = malloc((i + 2) * sizeof(char *));
+	if (new_env_array == NULL)
+		return (NULL);
+	i = 0;
+	while (env[i])
+	{
+		new_env_array[i] = env[i];
+		i++;
+	}
+	new_env_array[i] = ft_strdup(var);
+	if (new_env_array[i] == NULL)
+	{
+		free(new_env_array);
+		return (NULL);
+	}
+	new_env_array[i + 1] = NULL;
+	free(env);
+	return (new_env_array);
+}
+
+static char	*find_biggest_env_var(char **env)
+{
+	int		i;
+	char	*biggest_var;
+
+	if (env == NULL)
+		return (NULL);
+	i = 0;
+	biggest_var = env[0];
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], biggest_var, ft_strlen(biggest_var)) > 0)
+			biggest_var = env[i];
+		i++;
+	}
+	return (biggest_var);
+}
+
+static int	print_formatted_env_var(char *var)
+{
+	char	*var_to_print;
+	int		i;
+	int		temp;
+
+	i = 0;
+	var_to_print = ft_calloc(ft_strlen(var) + 4, 1);
+	if (var_to_print == NULL)
+		return (CRIT_ERROR);
+	while (var[i] != '=')
+		i++;
+	ft_memcpy(var_to_print, var, i + 1);
+	var_to_print = free_strjoin(var_to_print, "\"");
+	if (var_to_print == NULL)
+		return (CRIT_ERROR);
+	var_to_print = free_strjoin(var_to_print, &var[i + 1]);
+	if (var_to_print == NULL)
+		return (CRIT_ERROR);
+	var_to_print = free_strjoin(var_to_print, "\"\n");
+	if (var_to_print == NULL)
+		return (CRIT_ERROR);
+	temp = write(1, var_to_print, ft_strlen(var_to_print));
+	free(var_to_print);
+	if (temp == FAILURE)
+		return (ERROR);
+	return (SUCCESS);
+}
+
+static int	print_sorted_env(char **env)
 {
 	char	*smaller_var;
 	char	*var_to_print;
 	int		i;
 	int		y;
-	int		array_len;
+	int		error;
 
-	array_len = count_array_len(env);
-	y = 0;
+	y = count_array_len(env);
 	smaller_var = "\1";
-	var_to_print = env[0];
-	while (y < array_len)
+	while (y > 0)
 	{
+		var_to_print = find_biggest_env_var(env);
 		i = 0;
 		while (env[i])
 		{
-			if (ft_strncmp(env[i], smaller_var, 1000) > 0 && ft_strncmp(env[i], var_to_print, 1000) < 0)
+			if (ft_strncmp(env[i], smaller_var, ft_strlen(smaller_var)) > 0 && ft_strncmp(env[i], var_to_print, ft_strlen(var_to_print)) < 0)
 				var_to_print = env[i];
 			i++;
 		}
-		y++;
-		if (write(1, var_to_print, ft_strlen(var_to_print)) == FAILURE)
-			return (return_perror("bash: export", ERROR));
-		write(1, "\n", 1);
+		y--;
+		error = print_formatted_env_var(var_to_print);
+		if (error)
+			return (error);
 		smaller_var = var_to_print;
 	}
 	return (SUCCESS);
 }
 
-// int	ms_export(char **args, char **env)
-// {
-// 	int	i;
-// 	int	temp;
+int	ms_export(char **args, t_shell_vars *vars)
+{
+	int	i;
+	int	temp;
 
-// 	if (args[0] == NULL)
-// 	{
-// 		temp = print_sorted_env(env);
-// 		if (temp == CRIT_ERROR)
-// 			return (return_perror("bash: export critical error", CRIT_ERROR));
-// 		if (temp == ERROR)
-// 			return (return_perror("bash export", ERROR));
-// 		return (SUCCESS);
-// 	}
-// 	i = 0;
-// 	while (args[i])
-// 	{
-// 		env = add_var_to_env(env, args[i]);
-// 		if (env == NULL)
-// 			return (return_perror("bash: export critical error", CRIT_ERROR));
-// 		i++;
-// 	}
-// 	return (SUCCESS);
-// }
+	if (args[0] == NULL)
+	{
+		temp = print_sorted_env(vars->env);
+		if (temp == CRIT_ERROR)
+			return (return_perror("bash: export critical error", CRIT_ERROR));
+		if (temp == ERROR)
+			return (return_perror("bash export", ERROR));
+		return (SUCCESS);
+	}
+	i = 0;
+	while (args[i])
+	{
+		vars->env = add_var_to_env(vars->env, args[i]);
+		if (vars->env == NULL)
+			return (return_perror("bash: export critical error", CRIT_ERROR));
+		i++;
+	}
+	return (SUCCESS);
+}
 
 int	main(int ac, char *av[], char *envp[])
 {
-	print_sorted_env(envp);
+	t_shell_vars	vars;
+	t_error	error;
+
+	vars.env = init_env_array(envp, &error);
+	ms_export(&av[2], &vars);
+	if (ac >= 3)
+		print_sorted_env(vars.env);
+	free_array(vars.env);
 }
