7ed8f06b66d7b5aac11084df61a6f0542a35c2ee
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-05-02 16:21:31 +0200

    implementing cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,20 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecasalin <ecasalin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ecasalin <ecasalin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 07:21:49 by ecasalin          #+#    #+#             */
-/*   Updated: 2025/04/24 07:32:33 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/05/02 16:16:33 by ecasalin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "../../libft/src/libft.h"
 #include "../minishell.h"
+#include "../general_utils/utils.h"
+#include "../error_handling/errors.h"
 
-int	ms_cd(char *path, t_shell_vars *vars)
+char	*set_path_to_home(char **env)
 {
-	
+	char	*path;
+
+	path = get_env_var_value("HOME", env);
+	if (path == NULL)
+		ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+	return (path);
+}
+
+char	*set_oldpwd_to_home(char **env)
+{
+	char	*path;
+
+	path = get_env_var_value("OLDPWD", env);
+	if (path == NULL)
+		ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+	return (path);
+}
+
+int	ms_cd(char **args, t_shell_vars *vars)
+{
+	int		args_number;
+	char	*path;
+
+	args_number = count_array_len(args);
+	if (args_number > 1)
+		return (return_error("minishell: cd: to many arguments", ERROR));
+	else if (args_number == 0)
+		path = set_path_to_home(vars->env);
+	else if (ft_strncmp(args[0], "-", 2) == SUCCESS)
+		path = get_env_var_value("OLDPWD", vars->env);
+	if (path == NULL)
+		return (ERROR);
+	//si pas de . .. ou / au debut, on concat le premier path present dans CDPATH (rajouter un slash si pas present dans le CDPATH) avec path.
+	//si c'est un path valide on garde lui, sinon on concat avec ./
+	//si c'est valide on le garde, sinon on concat avec tous les paths restants dans CDPATH et on garder le premier valide.
+	//si aucun n'est valide on print erreur minishell: cd: "path": No such file or directotry
+	//si on trouve un path grace a CDPATH on le print
 	if (ft_strrchr(path, '/'))
-	
+
 }
--- src/error_handling/errors.h
+++ src/error_handling/errors.h
@@ -6,7 +6,7 @@
 /*   By: ecasalin <ecasalin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 16:47:33 by ecasalin          #+#    #+#             */
-/*   Updated: 2025/05/01 17:27:10 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/05/02 14:25:39 by ecasalin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 
 void	*set_err_return_null(t_error *error, t_error err_value);
 int		return_perror(char *err_msg, int return_value);
+int		return_error(char *err_msg, int return_value);
 
 #endif
