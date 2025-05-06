f708cdf5d9b9149b05f1a6ff4327724126a302c0
jules <142106821+julesportier@users.noreply.github.com>
2025-05-06 12:27:30 +0200

    feat: function to print the tokens list
    Added three helpers functions to access tokens list content without
    casting (see tokens_list_get.c)

---
+++ src/parsing/tokens_list_get.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens_list_get.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 10:59:37 by juportie          #+#    #+#             */
+/*   Updated: 2025/05/06 11:51:44 by juportie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+// /!\ The three functions below must be called after checking
+// that the tokens list is not NULL.
+enum e_token_type	get_toklist_type(t_dlst *list)
+{
+	return (((t_word *)(list->content))->type);
+}
+
+char	*get_toklist_str(t_dlst *list)
+{
+	return (((t_word *)(list->content))->str);
+}
+
+t_bool	get_toklist_cat_prev(t_dlst *list)
+{
+	return (((t_word *)(list->content))->cat_prev);
+}
