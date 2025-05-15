124032ae7a3a7badd2103c1010020f2f1eb89b5a
Erik Casalini <ecasalin@z2r8p8.42lyon.fr>
2025-05-15 15:41:33 +0200

    feat: added cd to builtins.h

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -17,6 +17,7 @@
 int		ms_echo(char **args);
 int		ms_export(char **args, t_shell_vars *vars);
 int		ms_pwd(void);
+int		ms_cd(char **args, t_shell_vars *vars);
 
 /*EXPORT UTILS*/
 char	*find_biggest_env_var(char **env);
