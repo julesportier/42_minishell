aaa2ff78ef20f309f1fa29fcddf7085b93b301eb
Erik Casalini <ecasalin@z2r2p5.42lyon.fr>
2025-05-21 08:13:32 +0200

    fix: fixed bad prototype of set_err_return_err in error.h

--- src/error_handling/errors.h
+++ src/error_handling/errors.h
@@ -6,7 +6,7 @@
 /*   By: ecasalin <ecasalin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 16:47:33 by ecasalin          #+#    #+#             */
-/*   Updated: 2025/05/19 11:08:59 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/05/21 08:12:28 by ecasalin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*set_err_return_null(t_error *error, t_error err_value);
 int		return_perror(char *err_msg, int return_value);
 int		return_error(char *err_msg, int return_value);
 int		exit_perror(char *err_msg, int exit_value);
-void	*set_err_return_err(t_error *error, t_error err_value);
+int		set_err_return_err(t_error *error, t_error err_value);
 int		return_perror_set_err(char *err_msg, t_error *error, t_error err_value);
 
 #endif
