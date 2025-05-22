4aea7ad81e41e87a61149d95ff5c0e63ae20497c
jules <142106821+julesportier@users.noreply.github.com>
2025-05-22 16:19:49 +0200

    feat: clearer syntax error message

--- src/parsing/error_print.c
+++ src/parsing/error_print.c
@@ -6,7 +6,7 @@
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 17:29:09 by juportie          #+#    #+#             */
-/*   Updated: 2025/05/06 17:30:22 by juportie         ###   ########.fr       */
+/*   Updated: 2025/05/22 11:25:15 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,13 @@ t_error	print_syntax_error(char *message, enum e_token_type type, t_error errnum
 	error = ft_strjoin("minishell: syntax error: ", message);
 	if (error == NULL)
 		return (errnum);
+	error = free_strjoin(error, "`", true, false);
+	if (error == NULL)
+		return (errnum);
 	error = free_strjoin(error, (char *)token_type_to_str(type), true, false);
+	if (error == NULL)
+		return (errnum);
+	error = free_strjoin(error, "'", true, false);
 	if (error == NULL)
 		return (errnum);
 	ft_putendl_fd(error, 2);
