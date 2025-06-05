4a811f8ee1a2f5838e320472eb2541afaef9d34b
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-06-05 18:03:23 +0200

    feat: script kind of handle heredocs and redirections

--- test/tester/tester.sh
+++ test/tester/tester.sh
@@ -4,10 +4,13 @@ input_file=test/tester/inputs/input_file
 ms_output_file=test/tester/outputs/ms_output_file
 bash_output_file=test/tester/outputs/bash_output_file
 diff_log=test/tester/outputs/diff_log
+redir_folder=test/tester/redirs
 
 >"$ms_output_file"
 >"$bash_output_file"
 >"$diff_log"
+
+rm -f $redir_folder/*
 
 while IFS='' read -r cmd || [ -n "$cmd" ]; do
 	printf 'Command: %s\n' "$cmd" >>$ms_output_file
@@ -24,8 +27,20 @@ while IFS='' read -r cmd || [ -n "$cmd" ]; do
 	printf '%s\n' "$cmd"
 	fi | ./minishell 2>&1 | grep -v '>' >>$ms_output_file
 	echo Exit value: ${PIPESTATUS[1]} >>$ms_output_file
+	shopt -s nullglob
+	redirs=($redir_folder/*)
+	if [[ ${redirs[@]} > 0 ]]; then
+		echo New files ↓
+		for file in "${redirs[@]}"; do
+		printf '%s : %s\n' "$file" "$(cat -A "$file")"
+		done
+	fi >>$ms_output_file
+	shopt -u nullglob
+	rm -f $redir_folder/*
 	echo >>$ms_output_file
 done <$input_file
+
+rm -f $redir_folder/*
 
 while IFS='' read -r cmd || [ -n "$cmd" ]; do
 	printf 'Command: %s\n' "$cmd" >>$bash_output_file
@@ -42,6 +57,16 @@ while IFS='' read -r cmd || [ -n "$cmd" ]; do
 	printf '%s\n' "$cmd"
 	fi | bash 2>&1 | grep -v '>' >>$bash_output_file
 	echo Exit value: ${PIPESTATUS[1]} >>$bash_output_file
+	shopt -s nullglob
+	redirs=($redir_folder/*)
+	if [[ ${redirs[@]} > 0 ]]; then
+		echo New files ↓
+		for file in "${redirs[@]}"; do
+		printf '%s : %s\n' "$file" "$(cat -A "$file")"
+		done
+	fi >>$bash_output_file
+	shopt -u nullglob
+	rm -f $redir_folder/*
 	echo >>$bash_output_file
 done <$input_file
 
@@ -49,9 +74,9 @@ done <$input_file
 diff -U 2 "$bash_output_file" "$ms_output_file" | grep -vE '^(---|\+\+\+|@@ )' > "$diff_log"
 
 if [ ${PIPESTATUS[0]} -eq 0 ]; then
-echo Success
-exit 0
+	echo Success
+	exit 0
 else
-cat $diff_log
-exit 1
+	cat $diff_log
+	exit 1
 fi
