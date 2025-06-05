0ac3736a3f06732cd3d62dd9c6a32e1433efb341
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-06-05 16:10:14 +0200

    script: added script tester

---
+++ test/tester/tester.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+input_file=test/tester/inputs/input_file
+ms_output_file=test/tester/outputs/ms_output_file
+bash_output_file=test/tester/outputs/bash_output_file
+diff_log=test/tester/outputs/diff_log
+
+>"$ms_output_file"
+>"$bash_output_file"
+>"$diff_log"
+
+while IFS='' read -r cmd || [ -n "$cmd" ]; do
+	printf 'Command: %s\n' "$cmd" >>$ms_output_file
+	if [[ "$cmd" == *"<<"* ]]; then
+		limiter=$(printf '%s' "$cmd" | grep -oP '<<\s*\K([^\s]+)')
+		printf '%s\n' "$cmd"
+		while read -r cmd; do
+			printf '%s\n' "$cmd"
+			if [[ "$cmd" == "$limiter" ]]; then
+			break
+			fi
+		done
+	else
+	printf '%s\n' "$cmd"
+	fi | ./minishell 2>&1 | grep -v '>' >>$ms_output_file
+	echo Exit value: ${PIPESTATUS[1]} >>$ms_output_file
+	echo >>$ms_output_file
+done <$input_file
+
+while IFS='' read -r cmd || [ -n "$cmd" ]; do
+	printf 'Command: %s\n' "$cmd" >>$bash_output_file
+	if [[ "$cmd" == *"<<"* ]]; then
+		limiter=$(printf '%s' "$cmd" | grep -oP '<<\s*\K([^\s]+)')
+		printf '%s\n' "$cmd"
+		while read -r cmd; do
+			printf '%s\n' "$cmd"
+			if [[ "$cmd" == "$limiter" ]]; then
+			break
+			fi
+		done
+	else
+	printf '%s\n' "$cmd"
+	fi | bash 2>&1 | grep -v '>' >>$bash_output_file
+	echo Exit value: ${PIPESTATUS[1]} >>$bash_output_file
+	echo >>$bash_output_file
+done <$input_file
+
+#diff -U 2 --label '' --label '' $bash_output_file $ms_output_file > $diff_log
+diff -U 2 "$bash_output_file" "$ms_output_file" | grep -vE '^(---|\+\+\+|@@ )' > "$diff_log"
+
+if [ ${PIPESTATUS[0]} -eq 0 ]; then
+echo Success
+exit 0
+else
+cat $diff_log
+exit 1
+fi
