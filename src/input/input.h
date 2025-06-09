1b4428f41f898762ab5134e78d4d2eff71e1351a
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-06-09 11:01:10 +0200

    fix: daltonian trap set

--- src/input/input.h
+++ src/input/input.h
@@ -6,19 +6,25 @@
 /*   By: ecasalin <ecasalin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 11:16:48 by juportie          #+#    #+#             */
-/*   Updated: 2025/06/09 09:50:13 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/06/09 10:51:36 by ecasalin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INPUT_H
 # define INPUT_H
 
+# ifndef PROMPT_SUCCESS
+#  define PROMPT_SUCCESS " \033[1;32m❯\033[0m "
+# endif
+# ifndef PROMPT_FAILURE
+#  define PROMPT_FAILURE " \033[1;31m❯\033[0m "
+# endif
+
 # include "../minishell.h"
 
 /*INPUT LOOP*/
 void	input_loop(t_shell_vars *vars, t_error *error);
 char	*set_readline_and_history(char *prompt, t_error *error);
-void	create_prompt(t_shell_vars *vars,
-			char *personalized_prompt, t_error *error);
+void	create_prompt(t_shell_vars *vars, t_error *error);
 
 #endif
