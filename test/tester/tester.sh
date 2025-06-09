1469f6ae088f6119a69590980f08da5fedb1153d
Erik <153425818+ErikCasalini@users.noreply.github.com>
2025-06-09 15:18:21 +0200

    fix: updated tests

--- test/tester/tester.sh
+++ test/tester/tester.sh
@@ -6,6 +6,9 @@ bash_output_file=test/tester/outputs/bash_output_file
 diff_log=test/tester/outputs/diff_log
 redir_folder=test/tester/redirs
 
+touch "$ms_output_file"
+touch "$bash_output_file"
+
 >"$ms_output_file"
 >"$bash_output_file"
 >"$diff_log"
@@ -14,27 +17,7 @@ rm -f $redir_folder/*
 
 while IFS='' read -r cmd || [ -n "$cmd" ]; do
 	printf 'Command: %s\n' "$cmd" >>$ms_output_file
-	 if [[ "$cmd" == *"<<"* ]]; then
-		limiter=$(printf '%s' "$cmd" | grep -oP '<<\s*\K([^\s]+)')
-		if [[ "$limiter" =~ ^\'.*\'$ ]]; then
-			limiter="${limiter#\'}"
-			limiter="${limiter%\'}"
-		elif [[ "$limiter" =~ ^\".*\"$ ]]; then
-			limiter="${limiter#\"}"
-			limiter="${limiter%\"}"
-		fi
-		printf '%s\n' "$cmd"
-		while read -r cmd; do
-			printf '%s\n' "$cmd"
-			printf '%s\n' "$cmd" >>$ms_output_file
-			if [[ "$cmd" == "$limiter" ]]; then
-				echo ↓ >>$ms_output_file
-				break;
-			fi
-		done
-	else
-	printf '%s\n' "$cmd"
-	fi | ./minishell 2>&1 | grep -v '>' >>$ms_output_file
+	printf '%s\n' "$cmd" | ./minishell 2>&1 | grep -v '>' >>$ms_output_file
 	echo Exit value: ${PIPESTATUS[1]} >>$ms_output_file
 	shopt -s nullglob
 	redirs=($redir_folder/*)
@@ -53,27 +36,7 @@ rm -f $redir_folder/*
 
 while IFS='' read -r cmd || [ -n "$cmd" ]; do
 	printf 'Command: %s\n' "$cmd" >>$bash_output_file
-	 if [[ "$cmd" == *"<<"* ]]; then
-		limiter=$(printf '%s' "$cmd" | grep -oP '<<\s*\K([^\s]+)')
-		if [[ "$limiter" =~ ^\'.*\'$ ]]; then
-			limiter="${limiter#\'}"
-			limiter="${limiter%\'}"
-		elif [[ "$limiter" =~ ^\".*\"$ ]]; then
-			limiter="${limiter#\"}"
-			limiter="${limiter%\"}"
-		fi
-		printf '%s\n' "$cmd"
-		while read -r cmd; do
-			printf '%s\n' "$cmd"
-			printf '%s\n' "$cmd" >>$bash_output_file
-			if [[ "$cmd" == "$limiter" ]]; then
-				echo ↓ >>$bash_output_file
-				break;
-			fi
-		done
-	else
-	printf '%s\n' "$cmd"
-	fi | bash 2>&1 | grep -v '>' >>$bash_output_file
+	printf '%s\n' "$cmd" | bash 2>&1 | grep -v '>' >>$bash_output_file
 	echo Exit value: ${PIPESTATUS[1]} >>$bash_output_file
 	shopt -s nullglob
 	redirs=($redir_folder/*)
