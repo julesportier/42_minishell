13337710a5e6de5e8da333f6b1ed16a5197d394b
Jules Portier <juportie@z2r9p1.42lyon.fr>
2025-06-16 10:21:25 +0200

    fix: error messages

--- src/error_handling/error_utils_1.c
+++ src/error_handling/error_utils_1.c
@@ -6,7 +6,7 @@
 /*   By: ecasalin <ecasalin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 16:35:53 by ecasalin          #+#    #+#             */
-/*   Updated: 2025/06/01 10:39:58 by ecasalin         ###   ########.fr       */
+/*   Updated: 2025/06/16 10:15:20 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,17 @@ int	return_perror(char *err_msg, int return_value)
 t_error	return_perror_set_err(char *err_msg, t_error *error, t_error err_value)
 {
 	perror(err_msg);
-	*error = err_value;
+	if (error)
+		*error = err_value;
 	return (err_value);
+}
+
+void	*return_perror_set_err_null(char *err_msg, t_error *error, t_error err_value)
+{
+	perror(err_msg);
+	if (error)
+		*error = err_value;
+	return (NULL);
 }
 
 int	return_error(char *err_msg, int return_value)
